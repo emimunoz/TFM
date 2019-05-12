b459e3a34810218a819729fcbaf199e3bebaa450
emimunoz <45298009+emimunoz@users.noreply.github.com>
2019-05-12 11:36:31 +0200

    Add files via upload

--- Codigo.R
+++ Codigo.R
@@ -26,7 +26,7 @@ accidentes_2016 <-  as_tibble(accidentes_2016)
 accidentes_2017 <-  as_tibble(accidentes_2017) 
 accidentes_2018 <-  as_tibble(accidentes_2018) 
 
-### Borrado de columnas innecesarias:-------------------------
+### Borrado de columnas innecesarias:------------
 
 # Hay algunas columnas que no van a ser necesarias para el estudio, 
 # por lo que lo mejor es deshacerse de ellas cuanto antes.
@@ -35,21 +35,42 @@ accidentes_2016 <- accidentes_2016 %>% select(-c(Codi_districte, Codi_barri, Cod
 accidentes_2017 <- accidentes_2017 %>% select(-c(Codi_districte, Codi_barri, Codi_carrer, Dia_setmana, Nom_mes))
 accidentes_2018 <- accidentes_2018 %>% select(-c(Codi_districte, Codi_barri, Codi_carrer, Dia_setmana, Nom_mes))
 
-### Traducción de los valores al español: ----------------------------------
+### Traducción de los valores al español: ------------------------------------------------------------------------------------------
 
 # Antes de comenzar a trabajar sobre los duplicados vamos a traducir al español los valores
 # para tener una mejor comprensión del DataSet, ya que está todo en catalán.
 str(accidentes_2016)
 
 # Renombrar columnas de "accidentes_2016":
-accidentes_2016 %>% 
+accidentes_2016 <- accidentes_2016 %>% 
   rename(
-    Numero_expedient = Num_exp, Nom_districte = Distrito, Nom_barri = Barrio, Nom_carrer = Calle, Num_postal = Cod_postal, 
-    Descripcio_dia_setmana = Dia_semana,  Descripcio_tipus_dia = Dia_tipo, Any = Año, Mes_any = Mes, Dia_mes = Dia, Hora_dia = Hora, 
-    Descripcio_torn = Momento_dia, Descripcio_causa_vianant = Causa_acc, Numero_morts 
-  )
+    Num_exp=Numero_expedient, Distrito=Nom_districte, Barrio=Nom_barri, Calle=Nom_carrer, Cod_postal=Num_postal, 
+    Dia_semana=Descripcio_dia_setmana,  Dia_tipo=Descripcio_tipus_dia, Año=Any, Mes=Mes_any, Dia=Dia_mes, Hora=Hora_dia, 
+    Momento_dia=Descripcio_torn, Causa_acc=Descripcio_causa_vianant, Num_muertos=Numero_morts, Num_les_leves=Numero_lesionats_lleus,
+    Num_les_graves=Numero_lesionats_greus, Num_victimas=Numero_victimes, Num_vehiculos_impl=Numero_vehicles_implicats)
 
-View(accidentes_2016)
+# Renombrar columnas de "accidentes_2017":
+accidentes_2017 <- accidentes_2017 %>% 
+  rename(
+    Num_exp=Numero_expedient, Distrito=Nom_districte, Barrio=Nom_barri, Calle=Nom_carrer, Cod_postal=Num_postal, 
+    Dia_semana=Descripcio_dia_setmana,  Dia_tipo=Descripcio_tipus_dia, Año=Any, Mes=Mes_any, Dia=Dia_mes, Hora=Hora_dia, 
+    Momento_dia=Descripcio_torn, Causa_acc=Descripcio_causa_vianant, Num_muertos=Numero_morts, Num_les_leves=Numero_lesionats_lleus,
+    Num_les_graves=Numero_lesionats_greus, Num_victimas=Numero_victimes, Num_vehiculos_impl=Numero_vehicles_implicats)
+
+# Renombrar columnas de "accidentes_2018":
+accidentes_2018 <- accidentes_2018 %>% 
+  rename(
+    Num_exp=Numero_expedient, Distrito=Nom_districte, Barrio=Nom_barri, Calle=Nom_carrer, Cod_postal=Num_postal, 
+    Dia_semana=Descripcio_dia_setmana,  Dia_tipo=Descripcio_tipus_dia, Año=Any, Mes=Mes_any, Dia=Dia_mes, Hora=Hora_dia, 
+    Momento_dia=Descripcio_torn, Causa_acc=Descripcio_causa_vianant, Num_muertos=Numero_morts, Num_les_leves=Numero_lesionats_lleus,
+    Num_les_graves=Numero_lesionats_greus, Num_victimas=Numero_victimes, Num_vehiculos_impl=Numero_vehicles_implicats)
+
+
+# Ahora vamos a traducir los valores que pueden tomar las variables del catalán al español. Por ejemplo, en vez de indicar en la 
+# columna Dia_semana 'Dimecres' que muestre 'Miercoles'.
+
+# Renombrar valores de "accidentes_2016":
+
 
 ### Comprobación de missing values: ----------
 

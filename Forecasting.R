b60d2c960a26d30afa6fdce66d7c6e3755c31998
emimunoz <45298009+emimunoz@users.noreply.github.com>
2020-04-19 12:52:03 +0200

    Add files via upload

--- Forecasting.R
+++ Forecasting.R
@@ -17,19 +17,51 @@ library(lubridate)
 library(plyr)
 library(tseries)
 
-accidentes <- read.csv("/Users/emi/Documents/Documentos/Data_Science/K_School/TFM/TFM_v3/accidentes_barcelona.csv", header = TRUE, sep = ";", stringsAsFactors = FALSE)
+### CREACIÓN DE LA SERIE TEMPORAL ----------------------------------------------
+
+accidentes_ds_completo <- read.csv("/Users/emi/Documents/Documentos/Data_Science/K_School/TFM/TFM_v3/accidentes_barcelona.csv", header = TRUE, sep = ";", stringsAsFactors = FALSE)
 
 # Al importar se ha perdido la clase 'fecha' para la columna del dataset llamada Fecha.
-accidentes$Fecha <- as.Date(accidentes$Fecha)
+accidentes_ds_completo$Fecha <- as.Date(accidentes_ds_completo$Fecha)
 
 # Creamos una columna con el valor 1 para realizar después un sumatorio de accidentes por día
-accidentes_1 <- accidentes %>% 
+accidentes_1 <- accidentes_ds_completo %>% 
   arrange(Fecha) %>% 
   mutate(num_acc=1) 
-  
-# Ahora ya tenemos el dataset listo como una serie temporal para comenzar con el análisis del número de accidentes en cada día de los 5 años.
-accidentes_ts <- aggregate(accidentes_1["num_acc"], by=accidentes_1["Fecha"],sum)
-ts(accidentes_ts)
+
+# Se suman los accidentes de cada día
+accidentes_sum_dia <- aggregate(accidentes_1["num_acc"], by=accidentes_1["Fecha"],sum)
+
+# Quitamos la columna con la fecha para poder pasar a crear ya la serie temporal con todos los accidentes
+accidentes_sum_dia$Fecha <- NULL
+
+# Creamos la serie temporal con frecuencia diaria desde el día 1 de enero de 2014
+accidentes_ts <- ts(accidentes_sum_dia, start = c(2014,1,1) , frequency = 365)
+plot(accidentes_ts)
+plot(acf(accidentes_ts))
 
 
-adf.test(accidentes)
+### ANÁLISIS DE LA SERIE TEMPORAL ---------------------------------------------------------
+
+# Comprobamos si la serie temporal es estacionaria:
+#   H0: Tendencia estacionaria 
+#   H1: No estacionaria 
+kpss.test(accidentes_ts, null = "Trend") # 0.08934 > 0.05 -> No podemos rechazar H0, por lo que no hay evidencia de que la serie no sea estacionaria
+
+# Comprobamos la estacionalidad con un adf test:
+adf.test(accidentes_ts) # Es una serie temporal estacionaria.
+
+# Extraemos la estacionalidad, tendencia y error de los componentes de la serie temporal para poder visualizarlos
+accidentes_descomp <- decompose(accidentes_ts, type = "additive")
+plot(accidentes_descomp)
+# Tendencia: Se comprueba que hay una tendencia que va entre 26.5 y 28.5 accidentes diarios, un cambio de un 7%.
+# Estacionalidad: Hay una fuerte estacionalidad que se va repitiendo cada año con una forma muy similar al anterior.
+# Ruido: Tiene mucha amplitud ya que los valores varían entre 20 y -20 accidentes diarios.  
+
+
+# Obtenemos las gráficas de autocorrelación y autocorrelación parcial, y en ambas se puede observar que aunque no llega a haber
+# una correlación muy elevada (el valor máximo alcanza una correlación de 0.469), si se verifica la estacionalidad de la serie temporal
+acf(accidentes_ts)
+pacf(accidentes_ts)
+
+
